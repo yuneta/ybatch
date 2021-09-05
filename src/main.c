6d4c1624a0c933a020da11f9ec7ae46a846da7ff
Niyamaka <niyamaka@yuneta.io>
2021-09-05 20:23:17 +0200

    task authenticate preparada para otros servicios ademas de keycloak

--- src/main.c
+++ src/main.c
@@ -28,12 +28,14 @@ struct arguments
     int print_role;
     int repeat;                 /* repeat time, in seconds (0 remove subscription) */
     char *url;
+    char *auth_owner;
     char *realm_name;
     char *yuno_role;
     char *yuno_name;
     char *yuno_service;
 
-    char *token_endpoint;
+    char *auth_system;
+    char *auth_url;
     char *user_id;
     char *user_passw;
     char *jwt;
@@ -146,7 +148,9 @@ static char args_doc[] = "FILE";
 static struct argp_option options[] = {
 /*-name-------------key-----arg---------flags---doc-----------------group */
 {0,                 0,      0,          0,      "OAuth2 keys", 20},
-{"token_endpoint",  'e',    "ENDPOINT", 0,      "OAuth2 Token EndPoint (get now a jwt)", 20},
+{"auth_system",     'K',    "AUTH_SYSTEM",0,    "OAuth2 System (default: keycloak, get now a jwt)", 20},
+{"auth_url",        'k',    "AUTH_URL", 0,      "OAuth2 Server Url (get now a jwt)", 20},
+{"auth_owner",      'w',    "AUTH_OWNER",0,     "OAuth2 Owner (get now a jwt)", 20},
 {"user_id",         'x',    "USER_ID",  0,      "OAuth2 User Id (get now a jwt)", 20},
 {"user_passw",      'X',    "USER_PASSW",0,     "OAuth2 User Password (get now a jwt)", 20},
 {"jwt",             'j',    "JWT",      0,      "Jwt (previously got it)", 21},
@@ -189,9 +193,16 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
     struct arguments *arguments = state->input;
 
     switch (key) {
-    case 'e':
-        arguments->token_endpoint = arg;
+    case 'K':
+        arguments->auth_system = arg;
         break;
+    case 'k':
+        arguments->auth_url = arg;
+        break;
+    case 'w':
+        arguments->auth_owner = arg;
+        break;
+
     case 'x':
         arguments->user_id = arg;
         break;
@@ -309,7 +320,9 @@ int main(int argc, char *argv[])
     arguments.yuno_role = "yuneta_agent";
     arguments.yuno_name = "";
     arguments.yuno_service = "__default_service__";
-    arguments.token_endpoint = "";
+    arguments.auth_system = "keycloak";
+    arguments.auth_url = "";
+    arguments.auth_owner = "";
     arguments.user_id = "";
     arguments.user_passw = "";
     arguments.jwt = "";
@@ -354,12 +367,14 @@ int main(int argc, char *argv[])
         argvs[idx++] = param2;
     } else {
         json_t *kw_utility = json_pack(
-            "{s:{s:i, s:i, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s}}",
+            "{s:{s:i, s:i, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s, s:s}}",
             "global",
             "YBatch.repeat", arguments.repeat,
             "YBatch.verbose", arguments.verbose,
             "YBatch.path", path,
-            "YBatch.token_endpoint", arguments.token_endpoint,
+            "YBatch.auth_system", arguments.auth_system,
+            "YBatch.auth_url", arguments.auth_url,
+            "YBatch.auth_owner", arguments.auth_owner,
             "YBatch.user_id", arguments.user_id,
             "YBatch.user_passw", arguments.user_passw,
             "YBatch.jwt", arguments.jwt,
